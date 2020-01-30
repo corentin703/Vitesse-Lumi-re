9f10235b7181cf1765f7a6c56564f8e96cee8378
RicouJ <julie.ricou@etu.uca.fr>
2020-01-30 18:04:51 +0100

    Integration scene course

---
+++ Vitesse-Lumière Unity/Assets/Scripts/Racing/CheckPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class CheckPoint : MonoBehaviour
+{
+    public bool isTrigger = false;
+
+    void OnTriggerEnter(Collider collid)
+    {
+        if (collid.tag == "Playermesh") isTrigger = true;
+    }
+
+    public void disableTrigger()
+    {
+        isTrigger = false;
+    }
+}
---
+++ Vitesse-Lumière Unity/Assets/Scripts/Racing/Racing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Racing : MonoBehaviour
+{
+    [SerializeField]
+    CheckPoint[] checkPoints;
+
+    [SerializeField]
+    Text textNbTour;
+
+    bool finishedTour = false;
+
+    int nbTour;
+    
+    void Update()
+    {
+        checkIfFinished();
+    }
+
+    void checkIfFinished()
+    {
+        finishedTour = true;
+        foreach (CheckPoint CP in checkPoints)
+        {
+            if (CP.isTrigger == false) finishedTour = false;
+        }
+        if (finishedTour == true)
+        {
+            nbTour++;
+            textNbTour.text = nbTour.ToString();
+            foreach (CheckPoint CP in checkPoints)
+            {
+                CP.disableTrigger();
+            }
+        }
+    }
+}
