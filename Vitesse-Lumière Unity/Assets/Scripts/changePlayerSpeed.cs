3c3532841d009579556cde20cd52dbf0d0aea66c
RicouJ <julie.ricou@etu.uca.fr>
2020-02-20 15:09:29 +0100

    Curseur Vitesse Mode Tourisme

---
+++ Vitesse-Lumière Unity/Assets/Scripts/changePlayerSpeed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changePlayerSpeed : MonoBehaviour
+{
+    [SerializeField] Slider slider;
+    [SerializeField] GameObject player;
+
+    void Update()
+    {
+        if(Input.mouseScrollDelta.y > 0) slider.value++;
+        else if(Input.mouseScrollDelta.y < 0) slider.value--;
+    }
+
+    public void changeSpeed()
+    {
+        player.GetComponent<GameState>().PlayerSpeed = (int)slider.value;
+    }
+}
