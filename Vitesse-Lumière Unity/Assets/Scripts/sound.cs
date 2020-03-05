e4c415ce98fe1f4884e55c6d9919f50ba09a6603
RicouJ <julie.ricou@etu.uca.fr>
2020-03-05 11:39:26 +0100

    Musique suivant la vitesse

---
+++ Vitesse-Lumière Unity/Assets/Scripts/sound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound : MonoBehaviour
+{
+    [SerializeField] AudioSource music;
+    [SerializeField] Rigidbody player;
+    float playerSpeed;
+
+    void Update()
+    {
+        playerSpeed = player.velocity.x + player.velocity.y;
+        music.volume = 1 - playerSpeed/500;
+    }
+}
