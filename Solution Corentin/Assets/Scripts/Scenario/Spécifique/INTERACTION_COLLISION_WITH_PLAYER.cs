673c7cf77539a0db236d0e2d89f3c7d671f53687
Corentin VÉROT <corentin.verot@outlook.com>
2021-04-21 17:11:03 +0200

    Nettoyage

--- Solution Corentin/Assets/Scripts/Scenario/Spécifique/INTERACTION_COLLISION_WITH_PLAYER.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class INTERACTION_COLLISION_WITH_PLAYER : Action_Scenario_Etape
-{
-
-    override public void Update() { }
-}
